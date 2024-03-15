7fc918d85405ed81364f3260b04caad7bbfc0c14
AliSabet81 <sabet.farshid1381@gmail.com>
2024-03-15 21:34:19 +0330

    channel card

---
+++ src/Dashboard/Content/Channels/ChannelCard.tsx
@@ -0,0 +1,35 @@
+const imageUrl = "https://www.sekirothegame.com/content/dam/atvi/sekiro/gallery/Sekiro_01.jpg";
+
+const ChannelAvatar = ({ url }: { url: string }) => {
+  return (
+    <div className="channels-avatar-container">
+      <img src={url || imageUrl} width="100%" height="100%" alt="" />
+    </div>
+  );
+};
+
+export const ChannelCard = ({
+  title,
+  username,
+  isOnline,
+  avatarUrl,
+  navigateToChannelHandler,
+  id,
+}: {
+  title: string;
+  username: string;
+  isOnline: boolean;
+  avatarUrl: string;
+  navigateToChannelHandler: () => void;
+  id: number;
+}) => {
+  const handleNavigate = () => {
+    navigateToChannelHandler();
+  };
+
+  return (
+    <div className="channels-card" onClick={handleNavigate}>
+      <ChannelAvatar url={avatarUrl} />
+    </div>
+  );
+};
--- src/Dashboard/Content/Channels/Channels.tsx
+++ src/Dashboard/Content/Channels/Channels.tsx
@@ -1,29 +1,32 @@
+import { ChannelCard } from "./ChannelCard";
+
+// eslint-disable-next-line react-refresh/only-export-components
 export const dummyChannels = [
   {
     id: 1,
     title: "test",
-    avatarUrl: null,
+    avatarUrl: "",
     username: "Martin",
     isOnline: false,
   },
   {
     id: 2,
     title: "test1",
-    avatarUrl: null,
+    avatarUrl: "",
     username: "Marta",
     isOnline: false,
   },
   {
     id: 3,
     title: "test2",
-    avatarUrl: null,
+    avatarUrl: "",
     username: "Jack",
     isOnline: false,
   },
   {
     id: 4,
     title: "test3",
-    avatarUrl: null,
+    avatarUrl: "",
     username: "Anna",
     isOnline: false,
   },
@@ -40,6 +43,7 @@ export const Channels = () => {
           isOnline={channel.isOnline}
           avatarUrl={channel.avatarUrl}
           navigateToChannelHandler={() => {}}
+          id={channel.id}
         />
       ))}
     </div>
