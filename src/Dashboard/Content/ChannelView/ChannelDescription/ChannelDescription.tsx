3af81e2fccc430b240ef1cb1fbc8332af4bc095d
AliSabet81 <sabet.farshid1381@gmail.com>
2024-03-16 01:00:28 +0330

    channel description

--- src/Dashboard/Content/ChannelView/ChannelDescription/ChannelDescription.tsx
+++ src/Dashboard/Content/ChannelView/ChannelDescription/ChannelDescription.tsx
@@ -1,5 +1,21 @@
-export const ChannelDescription = () => {
+export const ChannelDescription = ({
+  channelId,
+  title,
+  description,
+  username,
+}:{
+  channelId:number,
+  title:string,
+  description:string,
+  username:string,
+}) => {
   return (
-    <div className="channel-description-container">ChannelDescription</div>
+    <div id={`${channelId}`} className="channel-description-container">
+      <span className="channel-description-title">{username}</span>
+      <span className="channel-description-subtitle">{title}</span>
+      <div className="channel-description-box">
+        <span className="channel-description">{description}</span>
+      </div>
+    </div>
   );
 };
