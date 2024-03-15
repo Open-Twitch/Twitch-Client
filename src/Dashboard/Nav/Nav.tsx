6f8701545127d0f2dc825f23d5587353ed48b79a
AliSabet81 <sabet.farshid1381@gmail.com>
2024-03-15 18:51:20 +0330

    Nav Logo component

--- src/Dashboard/Nav/Nav.tsx
+++ src/Dashboard/Nav/Nav.tsx
@@ -1,3 +1,33 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import { NavLogo } from ".";
+
+const NavButton = ({
+  text,
+  onClickHandler,
+}: {
+  text: string;
+  onClickHandler: any;
+}) => {
+  return (
+    <span className="nav-button" onClick={onClickHandler}>
+      {text}
+    </span>
+  );
+};
+
 export const Nav = () => {
-  return <div className="nav-container">Nav</div>;
+  return (
+    <div className="nav-container">
+      <NavLogo />
+      <div className="nav-buttons-container">
+        <NavButton text="Browse" onClickHandler={() => {}} />
+        <NavButton text="Login" onClickHandler={() => {}} />
+        <div>
+          <NavButton text="My Account" onClickHandler={() => {}} />
+          <NavButton text="Logout" onClickHandler={() => {}} />
+        </div>
+      </div>
+    </div>
+  );
 };
