fb82c008401000cd143328a81e8522a8f627a056
AliSabet81 <sabet.farshid1381@gmail.com>
2024-03-16 23:55:44 +0330

    Input textarea

--- src/shared/components/Input.tsx
+++ src/shared/components/Input.tsx
@@ -1,48 +1,59 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 export const Input = ({
-    field,
-    label,
-    value,
-    onChangeHandler,
-    type,
-    showErrorMessage,
-    validateMessage,
-    onBlurHandler,
-  }: {
-    field: string;
-    label: string;
-    value: string;
-    onChangeHandler: any;
-    type: string;
-    showErrorMessage: boolean;
-    validateMessage: string;
-    onBlurHandler: any;
-  }) => {
-      
-    const handleValueChange = (e: any) => {
-      onChangeHandler(e.target.value, field);
-    };
-  
-    const handleInputBlur = (e: any) => {
-      onBlurHandler(e.target.value, field);
-    };
-  
-    return (
-      <>
-        <div className="auth-form-label">
-          <span>{label}</span>
-        </div>
+  field,
+  label,
+  value,
+  onChangeHandler,
+  type,
+  showErrorMessage,
+  validateMessage,
+  onBlurHandler,
+  textarea,
+}: {
+  field: string;
+  label: string;
+  value: string;
+  onChangeHandler: any;
+  type: string;
+  showErrorMessage: boolean;
+  validateMessage: string;
+  onBlurHandler: any;
+  textarea?: boolean;
+}) => {
+  const handleValueChange = (e: any) => {
+    onChangeHandler(e.target.value, field);
+  };
+
+  const handleInputBlur = (e: any) => {
+    onBlurHandler(e.target.value, field);
+  };
+
+  return (
+    <>
+      <div className="auth-form-label">
+        <span>{label}</span>
+      </div>
+      {textarea ? (
+        <textarea
+          typeof={type}
+          value={value}
+          onChange={handleValueChange}
+          onBlur={handleInputBlur}
+          rows={5}
+          style={{ maxWidth: "400px" }}
+        ></textarea>
+      ) : (
         <input
           type={type}
           value={value}
           onChange={handleValueChange}
           onBlur={handleInputBlur}
         />
-        <span className="auth-form-validation-message">
-          {showErrorMessage && validateMessage}
-        </span>
-      </>
-    );
-  };
-  
+      )}
+      <span className="auth-form-validation-message">
+        {showErrorMessage && validateMessage}
+      </span>
+    </>
+  );
+};
