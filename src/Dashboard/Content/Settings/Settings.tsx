b17768693be993a8235471e54f74acdde22a1ac6
AliSabet81 <sabet.farshid1381@gmail.com>
2024-03-17 21:47:20 +0330

    replacing real channel setting data

--- src/Dashboard/Content/Settings/Settings.tsx
+++ src/Dashboard/Content/Settings/Settings.tsx
@@ -1,14 +1,6 @@
 import { LoadingSpiner, useChannelSetting } from "@/shared";
 import { ChannelSettings, PasswordSetting, StreamKey } from ".";
 
-const channelsSettings: { [key: string]: string } = {
-  title: "title",
-  description: "description",
-  avatarUrl: "none",
-  username: "Martin",
-  streamKey: "1234",
-};
-
 export const Settings = () => {
   const { channelsSettings,isFetching } = useChannelSetting();
 
